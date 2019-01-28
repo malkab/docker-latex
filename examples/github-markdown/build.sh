e74f7c9559394316bd01944466fdd9bf59fac028
Juan Pedro Pérez Alcántara <jp.perez.alcantara@gmail.com>
2019-01-28 10:15:58 +0100

    Upgrading to Pandoc 2.5
    Upgraded to Pandoc 2.5 and a little bit of experimentation and tidy up.
    New image built and pushed to repos, ready to use.
    
    **CHANGED**
    
    - Upgraded Pandoc to 2.5 from the binary provided by the Pandoc team,
      not installing from packages.
    
    - Tided up a little bit the examples.
    
    **FIXED**
    
    - Fixed a bug in the Dockerfile that prevented GhostScript from
      installing from packages properly.

--- examples/github-markdown/build.sh
+++ examples/github-markdown/build.sh
@@ -7,6 +7,27 @@ TEMPLATESDIR=/templates
 
 mkdir -p out
 
-$PANDOC src/example-github-markdown.md memoir.yaml -s --top-level-division=chapter --table-of-contents --number-sections --highlight-style=haddock --template="${TEMPLATESDIR}/Memoir/Memoir.tex" -o out/example-github-markdown.tex
 
-$PANDOC src/example-github-markdown.md memoir.yaml -s --top-level-division=chapter --table-of-contents --number-sections --highlight-style=haddock --template="${TEMPLATESDIR}/Memoir/Memoir.tex" -o out/example-github-markdown.pdf
+
+$PANDOC -s \
+    --table-of-contents \
+    --top-level-division=chapter \
+    --number-sections \
+    --highlight-style=haddock \
+    --include-in-header=src/header.tex \
+    --template="${TEMPLATESDIR}/Memoir/Memoir.tex" \
+    -o out/example-github-markdown.tex \
+    src/example-github-markdown.md \
+    memoir.yaml
+    
+
+$PANDOC -s \
+    --table-of-contents \
+    --number-sections \
+    --top-level-division=chapter \
+    --highlight-style=haddock \
+    --include-in-header=src/header.tex \
+    --template="${TEMPLATESDIR}/Memoir/Memoir.tex" \
+    -o out/example-github-markdown.pdf \
+    src/example-github-markdown.md \
+    memoir.yaml 
