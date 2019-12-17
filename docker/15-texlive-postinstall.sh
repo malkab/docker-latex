15b96e08902918c3857ef5d60e19605a329796bd
Juan Pedro Pérez Alcántara <jp.perez.alcantara@gmail.com>
2019-12-17 12:08:12 +0100

    WIP: configuration of memory prepared to create new image

--- docker/15-texlive-postinstall.sh
+++ docker/15-texlive-postinstall.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 # This script is meant to be run inside the 
-# text-workflows-install-container to configure paths
+# text-workflows-install-container to configure paths and memory
+
+
+# Paths
 
 echo MANPATH=$MANPATH:/usr/local/texlive/2019/texmf-dist/doc/man >> ~/.bashrc
 
 echo INFOPATH=$INFOPATH:/usr/local/texlive/2019/texmf-dist/doc/info >> ~/.bashrc
 
 echo PATH=$PATH:/usr/local/texlive/2019/bin/x86_64-linux >> ~/.bashrc
+
+. ~/.bashrc
+
+
+# Memory config
+
+cp /ext-src/assets/texmf.cnf /usr/local/texlive/2019/texmf.cnf
+
+fmtutil-sys --all
